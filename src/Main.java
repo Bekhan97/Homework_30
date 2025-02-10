2132079ca7ebbdf03feaa522cf83cd76e09b223e
Beckhan <Bekhan97@gmail.com>
2025-02-10 20:56:04 +0100

    Add Flyable list in Main class
    - Iterate through list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Duck duck1 = new Duck("Donald");
+        Aircraft aircraft = new Aircraft("Airbus");
+        Aerostat aerostat = new Aerostat("Aerostat");
+
+        List<Flyable> list = List.of(
+                duck1,
+                aircraft,
+                aerostat
+        );
+
+        for (Flyable flyable : list) {
+            flyable.fly();
+        }
     }
 }
